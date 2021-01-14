0f42c018141644a9bb6d8575fe317d0c1c864f85
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-01-15 01:39:05 +0545

    Created function to add new student

--- app/src/main/java/com/sayyed/softuser/fragments/StudentFragment.kt
+++ app/src/main/java/com/sayyed/softuser/fragments/StudentFragment.kt
@@ -5,29 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.Toast
 import com.sayyed.softuser.R
+import com.sayyed.softuser.model.StudentModel
+import com.sayyed.softuser.model.StudentsList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var etFullName : EditText
+    private lateinit var etAge : EditText
+    private lateinit var rdoMale : RadioButton
+    private lateinit var rdoFemale : RadioButton
+    private lateinit var rdoOthers : RadioButton
+    private lateinit var etAddress : EditText
+    private lateinit var btnSave : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +33,50 @@ class StudentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student, container, false)
+        val view = inflater.inflate(R.layout.fragment_student, container, false)
+
+        etFullName = view.findViewById(R.id.etFullName)
+        etAge = view.findViewById(R.id.etAge)
+        rdoMale = view.findViewById(R.id.rdoMale)
+        rdoFemale = view.findViewById(R.id.rdoFemale)
+        rdoOthers = view.findViewById(R.id.rdoOthers)
+        etAddress = view.findViewById(R.id.etAddress)
+        btnSave = view.findViewById(R.id.btnSave)
+
+        btnSave.setOnClickListener {
+            createStudent()
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun createStudent() {
+        val fullname = etFullName.text.toString()
+        val age = etAge.text.toString()
+        var gender = ""
+        val address = etAddress.text.toString()
+
+        when {
+            rdoMale.isChecked -> {
+                gender = "Male"
             }
+            rdoFemale.isChecked -> {
+                gender = "Female"
+            }
+            rdoOthers.isChecked -> {
+                gender = "Others"
+            }
+
+        }
+
+        val student = StudentModel(fullname, address, gender, age)
+
+        val studentToList = StudentsList()
+
+        studentToList.addStudentInList(student)
+
+        studentToList.printStudentList()
     }
+
+
 }
--- app/src/main/java/com/sayyed/softuser/model/StudentModel.kt
+++ app/src/main/java/com/sayyed/softuser/model/StudentModel.kt
@@ -1,3 +1,3 @@
 package com.sayyed.softuser.model
 
-data class StudentModel (var fullName: String, var address: String, var gender: String, var age: Int)
+data class StudentModel (var fullName: String, var address: String, var gender: String, var age: String)
