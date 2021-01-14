fc1de86d3ac74fc7fffdc6567840a702af88f20e
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-01-14 23:25:34 +0545

    Added dashboard activity

--- app/src/main/java/com/sayyed/softuser/MainActivity.kt
+++ app/src/main/java/com/sayyed/softuser/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.sayyed.softuser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etUsername: EditText
@@ -13,5 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+
+        val username = "admin"
+        val password = "admin"
+
+        btnLogin.setOnClickListener {
+            if (etUsername.text.toString() == username && etPassword.text.toString() == password) {
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Username and password did not matched!!", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }
