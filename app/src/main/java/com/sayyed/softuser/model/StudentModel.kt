b6915626ef2ef290214e3d3be335c061bde8cb86
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-01-15 09:27:04 +0545

    Created student layout for recycler view

--- app/src/main/java/com/sayyed/softuser/model/StudentModel.kt
+++ app/src/main/java/com/sayyed/softuser/model/StudentModel.kt
@@ -1,3 +1,9 @@
 package com.sayyed.softuser.model
 
-data class StudentModel (var fullName: String, var address: String, var gender: String, var age: String, var dp: String)
+data class StudentModel (
+    var fullName: String? = null,
+    var address: String? = null,
+    var gender: String? = null,
+    var age: String? = null,
+    var dp: String? = null
+)
