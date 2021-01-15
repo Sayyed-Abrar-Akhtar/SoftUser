b3cebf3874dbe8e206a8b7b77783bb2f639abad6
Sayyed-Abrar-Akhtar <akhtars10@uni.coventry.ac.uk>
2021-01-15 10:03:38 +0545

    Created recycler view in dashboard

--- app/src/main/java/com/sayyed/softuser/adapter/StudentAdapter.kt
+++ app/src/main/java/com/sayyed/softuser/adapter/StudentAdapter.kt
@@ -1,17 +1,20 @@
 package com.sayyed.softuser.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sayyed.softuser.R
 import com.sayyed.softuser.model.StudentModel
 import de.hdodenhof.circleimageview.CircleImageView
 
 class StudentAdapter (
 
-    val list : ArrayList<StudentModel>,
+    val listStudent : ArrayList<StudentModel>,
     val context: Context
 ) : RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
     class StudentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -30,6 +33,37 @@ class StudentAdapter (
             delStudent = view.findViewById(R.id.delStudent)
             tvGender = view.findViewById(R.id.tvGender)
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val view =LayoutInflater.from(parent.context).inflate(R.layout.student_layout, parent, false)
+        return StudentViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val students = listStudent[position]
+
+        Glide.with(context)
+            .load(students.dp)
+            .into(holder.studentImage)
+
+        holder.tvFullName.text =students.fullName
+        holder.tvAge.text =students.age
+        holder.tvAddress.text =students.address
+        holder.tvGender.text =students.gender
+
+        holder.delStudent.setOnClickListener{
+            deleteItem(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listStudent.size
+    }
+
+    fun deleteItem (index: Int) {
+        listStudent.removeAt(index)
+        notifyDataSetChanged()
     }
 
 }
